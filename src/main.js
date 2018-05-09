5798793276ebaf542f37ee7a79ec4aa8f2fd0dae
r-yanyo <honma.ryosuke@gmail.com>
2018-05-10 01:54:27 +0900

    Add onsen-ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
+// JS import
+import Vue from 'vue';
+import VueOnsen from 'vue-onsenui'; // This already imports 'onsenui'
+
 import App from './App.vue'
+
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+Vue.use(VueOnsen);
